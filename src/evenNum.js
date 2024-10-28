b3cd737384a122bd71a9b7abc7fdd052a5533b3b
NastyaSia04 <nastyasia04@gmail.com>
2024-10-28 16:43:33 +0200

    correct if and exit

--- src/evenNum.js
+++ src/evenNum.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 export default () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name?');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   const getRandomNumber = (min, max) => {
@@ -21,8 +21,8 @@ export default () => {
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer !== correctAnswer) {
-      console.log( `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}!`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      return console.log(`Let's try again, ${userName}!`);
     }
     console.log('Correct!');
   }
