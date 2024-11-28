037b52dd9eea26f1765a0217bc07af1e85f06f90
NastyaSia04 <nastyasia04@gmail.com>
2024-11-28 14:03:16 +0200

    deleted utils.js and moved to src

--- utils.js
+++
@@ -1,6 +0,0 @@
-const getRandomNumber = (minNum, maxNum) => {
-  const randomNumber = Math.floor(Math.random() * (maxNum - minNum) + minNum);
-  return randomNumber;
-};
-
-export default getRandomNumber;
